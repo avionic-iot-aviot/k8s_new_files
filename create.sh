62b5ef9d63f8c5c29626c3f9d00f1eadd4300343
Andrea Santangelo <asantangelo90@gmail.com>
2021-03-03 11:41:34 +0100

    Simplified folder names. Create and destroy scripts.

---
+++ create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+  echo "Usage: create.sh COM_NAME COM_PASSWORD SUPERNODE_URL NAMESPACE" >&2
+  exit 1
+fi
+
+kubectl create namespace $4
+
+COM_NAME=$1 COM_PASSWORD=$2 SUPERNODE_URL=$3 envsubst < configmap.yaml | kubectl apply -n $4 -f -
+kubectl apply -n $4 -f DnsServerAppComplete/dnsserverapp-complete.yaml
+kubectl apply -n $4 -f Frontend/dbapp-fe.yaml
+kubectl apply -n $4 -f MLVPN/MLVPN.yaml
+kubectl apply -n $4 -f MongoDB/mongodb.yaml
+kubectl apply -n $4 -f REDIS/redis.yaml
+kubectl apply -n $4 -f REST-API/rest-api.yaml
+kubectl apply -n $4 -f ROS-CORE/ros.yaml
+kubectl apply -n $4 -f ROS-NODE-JS/ros-node.yaml
+kubectl apply -n $4 -f VideoServer/janus.yaml
+kubectl apply -n $4 -f WebSoketServer/ws-server.yaml
+
---
+++ destroy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: destroy.sh NAMESPACE" >&2
+  exit 1
+fi
+
+kubectl delete configmap -n $1 configmap-aviot
+
+kubectl delete deployment -n $1 dbapp-fe
+kubectl delete deployment -n $1 dnsserverapp-complete
+kubectl delete deployment -n $1 janus-gateway
+kubectl delete deployment -n $1 mlvpn
+kubectl delete deployment -n $1 mongo
+kubectl delete deployment -n $1 redis
+kubectl delete deployment -n $1 restapi
+kubectl delete deployment -n $1 ros-master
+kubectl delete deployment -n $1 ros-node
+kubectl delete deployment -n $1 wss
+
+kubectl delete service -n $1 dbapp-fe
+kubectl delete service -n $1 dnsserverapp
+kubectl delete service -n $1 janus-gateway
+kubectl delete service -n $1 mlvpn
+kubectl delete service -n $1 mongo
+kubectl delete service -n $1 redis
+kubectl delete service -n $1 restapi
+kubectl delete service -n $1 ros-master
+kubectl delete service -n $1 ros-node
+kubectl delete service -n $1 wss
+
+
