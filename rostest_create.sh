9f518f68990be3c510cfcfba48ae1983c0e718a3
Andrea Santangelo <asantangelo90@gmail.com>
2021-07-21 14:31:39 +0200

    Created yaml and create and destroy scripts for rosnodejs testing.

---
+++ rostest_create.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DNS_SERVER_ADDRESS=$2 NUMBER=1 envsubst < ROS-TEST/ros-test.yaml | kubectl apply -n $1 -f -
+DNS_SERVER_ADDRESS=$2 NUMBER=2 envsubst < ROS-TEST/ros-test.yaml | kubectl apply -n $1 -f -
